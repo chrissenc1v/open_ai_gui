a7a2a6749f0fc6dd4e528628733b9752eb0ca124
CEJ Van Niekirk <cejvanniekirk098@gmail.com>
2023-01-16 09:41:05 -0600

    added doc to api response function

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ import customtkinter
 
 
 def api_response(prompt):
+    """
+    Generates a text response based on a given prompt using the OpenAI API.
+    
+    Parameters:
+        prompt (str): The text prompt that the API will use as the starting point for generating a response.
+    
+    Returns:
+        str: The generated text response.
+        
+    Example:
+        response = api_response("What is the weather like today?")
+        print(response)
+        # Output: "The weather today is sunny with a high of 75 degrees."
+    """
     openai.api_key = "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"
     # openai.api_key = os.environ("OpenAI_Key")
 
